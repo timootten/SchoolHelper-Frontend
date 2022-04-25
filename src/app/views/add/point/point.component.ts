bdca4a8b79ffd3810634015d5b2a9fe3902cae37
MirSowasVonEgal <timootten@icloud.com>
2022-04-25 18:20:52 +0200

    Updated

--- src/app/views/add/point/point.component.ts
+++ src/app/views/add/point/point.component.ts
@@ -10,25 +10,65 @@ export class PointComponent implements OnInit {
 
   days = new Map();
 
-  weekday = new Date().getDay();
+  weekday = new Date().getDay()+1;
 
-  weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag","Samstag"];
+  weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+
+  weekdaysRegular = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"  ];
 
   selected = this.weekdays[this.weekday];
 
+  subject: any;
+
+  number: Number | undefined;
+
   constructor(
-    firestore: AngularFirestore,
+    private firestore: AngularFirestore,
   ) {
     firestore.collection('schedule').get().subscribe(data => {
       data.forEach(doc => {
         const data: any = doc.data();
         this.days.set(doc.id, Object.keys(data));
       });
-      console.log(this.days.get('Freitag'))
     });
    }
 
   ngOnInit(): void {
+  }
+
+  numberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+
+  updateNumber(event: any): void {
+    this.number = event.target.value;
+  }
+
+  // get date from last selected weekday
+  getDate(): Date {
+
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    date.setDate(date.getDate() - this.weekdaysRegular.slice().reverse().indexOf(this.selected) - 1);
+    return date;
+  }
+
+  submit() {
+    if (this.selected === undefined || this.subject === undefined || this.number == undefined || this.number.toString() === '') return;
+
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    const data = { selected: this.selected, subject: this.subject, number: this.number, date: date };
+    this.firestore.collection("users").doc("Leon").set({
+      [date.toLocaleDateString('de-DE')]: { [this.subject]: data }
+    }, { merge: true }).then(() => {
+      alert("Erfolgreich gespeichert");
+    });
   }
 
 
--- src/app/views/calender/calender.component.ts
+++ src/app/views/calender/calender.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatCalendar, MatCalendarCellCssClasses } from '@angular/material/datepicker';
+import { AngularFirestore } from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-calender',
@@ -10,24 +11,56 @@ export class CalenderComponent implements OnInit {
 
   selected: Date | null = this.getDefaultDate();
 
-  constructor() { }
+  days: any = {};
+
+  info: string | undefined;
+
+
+  @ViewChild(MatCalendar, {static: false}) calendar: MatCalendar<Date> | undefined;
+
+  constructor(private firestore: AngularFirestore) {
+    // get from collection users and from doc Leon every collection
+
+  }
 
   ngOnInit(): void {
+    this.firestore.collection('users').doc("Leon").get().subscribe(result => {
+      this.days = result.data();
+      this.calendar?.updateTodaysDate();
+      this.updateText();
+    });
+  }
+
+  updateText() {
+    const day = this.selected?.toLocaleDateString('de-DE');
+    this.info = "Ausgewählter Tag: " + day;
+    if(day == undefined || !this.days[day]) return;
+    Object.entries(this.days[day]).forEach(subject => {
+      let obj: any = subject[1];
+      this.info += "\n" + obj.subject + ": " + obj.number;
+    });
   }
 
   dateClass() {
     return (date: Date): MatCalendarCellCssClasses => {
-      console.log(date)
-      if (date.getDate() === 1) {
-        return 'special-date';
-      } else {
-        return '';
+      if(this.days[date.toLocaleDateString('de-DE')]) {
+        var green = true;
+        const day = this.days[date.toLocaleDateString('de-DE')];
+        Object.entries(day).forEach(subject => {
+          let obj: any = subject[1];
+          if(obj.number < 2)
+            green = false;
+        });
+        if(green)
+          return 'green';
+        return 'red';
       }
+      return '';
     };
   }
 
   getDefaultDate(): Date {
-    var date = new Date();
+    const date = new Date();
     date.setHours(0, 0, 0, 0);
     return date;
   }
